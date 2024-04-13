213ca37d0665b2f01dfec41d1fb517eaf239a055
shivamrai27 <200shivamrai@gmail.com>
2024-04-13 21:49:06 +0530

    News Controller Store Method

--- controllers/NewsController.js
+++ controllers/NewsController.js
@@ -1,7 +1,29 @@
+import vine, { errors } from '@vinejs/vine';
+import { newsSchema } from '../Validations/NewsValidation.js';
+import { messages } from '@vinejs/vine/defaults';
 class NewsController {
     static async index(req, res) { }
 
-    static async store(req, res) { }
+    static async store(req, res) {
+        try {
+            const user = req.user;
+            const body = req.body;
+            const validator = vine.compile(newsSchema)
+            const payload = await validator.validate(body)
+        } catch (error) {
+
+            if (error instanceof errors.E_VALIDATION_ERROR) {
+                console.log(error.message);
+                return res.status(400).json({ errors: error.messages });
+            } else {
+                return res.status(500).json({
+                    status: 500,
+                    message: "something went wrong"
+                })
+            }
+        }
+
+    }
 
     static async show(req, res) { }
 
@@ -9,3 +31,5 @@ class NewsController {
 
     static async destroy(req, res) { }
 }
+
+export default NewsController;
--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import AuthController from "../controllers/AuthController.js";
 import authMiddleware from "../middleware/Authenticate.js";
 import ProfileController from "../controllers/ProfileController.js";
+import NewsController from "../controllers/NewsController.js";
 
 const router = Router()
 router.post("/auth/register", AuthController.register);
@@ -10,4 +11,11 @@ router.post("/auth/login", AuthController.login);
 // * Profile routes
 router.get("/profile", authMiddleware, ProfileController.index); //Private route
 router.put("/profile/:id", authMiddleware, ProfileController.update); //Private route
+
+// * News routes
+router.get("/news", NewsController.index);
+router.post("/news", authMiddleware, NewsController.store);
+router.get("/news/:id", NewsController.show);
+router.put("/news/:id", NewsController.update);
+router.delete("/news/:id", NewsController.destroy);
 export default router;
