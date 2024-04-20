a3e1f5bd10ab4ee73788c6325a1d7b76b0882f9f
shivamrai27 <200shivamrai@gmail.com>
2024-04-21 00:22:17 +0530

    api rate limiter is setup

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import express from "express";
 import "dotenv/config"
 const app = express();
 import fileUpload from "express-fileupload"
+import helmet from "helmet";
+import cors from "cors";
+import { limiter } from './config/ratelimiter.js'
 const PORT = process.env.PORT || 8000;
 
 // * Middleware
@@ -9,12 +12,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 app.use(fileUpload());
+app.use(helmet());
+app.use(cors());
+app.use(limiter)
+
 app.get("/", (req, res) => {
     return res.json({ message: "Hello Its working" });
 })
 
 // * Import routes
 import ApiRoutes from "./routes/api.js";
+import rateLimit from "express-rate-limit";
 app.use("/api", ApiRoutes);
 
 app.listen(PORT, () => console.log(`server is runing on ${PORT}`))
